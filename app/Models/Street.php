d530b034380b12d7a3c1941c6815b115ccc16978
Rômulo Neves <romulovneves@gmail.com>
2024-02-12 21:03:07 -0300

    Adicionando classe e seed de Steet (Rua)

--- app/Models/Street.php
+++ app/Models/Street.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class State extends Model
+class Street extends Model
 {
     use HasFactory;
 
@@ -16,6 +16,5 @@ class State extends Model
      */
     protected $fillable = [
         'name',
-        'uf'
     ];
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
 
         $this->call(StateSeeder::class);
         $this->call(CitySeeder::class);
+        $this->call(StreetSeeder::class);
     }
 }
